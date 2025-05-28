eafa7bb8aa5a4ba1211ee969b817296ab8d267be
hasinduudara <hasindutechschool@gmail.com>
2025-05-28 21:53:54 +0530

    Initial commit

---
+++ src/main/java/org/example/StudentRegisterServlet.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/studentRegister")
+public class StudentRegisterServlet extends HttpServlet {
+}
